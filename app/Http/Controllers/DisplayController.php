ca3562f786ce5597a2ad33f6e1e7b2942a6709b8
bayuyuhartono-katadata <bayu.yuhartono@katadata.co.id>
2021-05-15 01:58:28 +0700

    first

---
+++ app/Http/Controllers/DisplayController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DisplayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($company_name)
+    {
+        return view('display.main', [
+            'company_name' => $company_name
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DisplayController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\BuilderController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// route with subdomain
+Route::group(['domain' => '{company_name}.localhost'], function ($company_name) {
+    // Route::get('/', 'DisplayController@index($company_name)')->name('display');
+    Route::get('/', [DisplayController::class, 'index', $company_name]);
+});
+
+Route::get('/', [DashboardController::class, 'index']);
+Route::get('/builder', [BuilderController::class, 'index']);
